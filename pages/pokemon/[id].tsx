64d1b8624f68c58d0bb428571f1fe438e77c5e91
Ieremy Flores <eulerfermatleibniz@gmail.com>
2023-05-04 01:34:13 -0500

    remove mistake

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -61,7 +61,13 @@ const PokemonPage: NextPage<Props> = ({ pokemon }) => {
         <Grid xs={12} sm={8}>
           <Card>
             <Card.Header
-              css={{ display: 'flex', justifyContent: 'space-between' }}
+              css={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                '@xsMax': {
+                  flexDirection: 'column',
+                },
+              }}
             >
               <Text h1 transform='capitalize'>
                 {pokemon.name}
